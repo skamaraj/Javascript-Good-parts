4cfe2e22f2c6285e34c39592fcb41ee8ede2d188
snigdha <snigdhakumari7@gmail.com>
2014-03-04 15:13:48 -0500

    pseudoclassical inheritence

--- AugmentingFunctionType.js
+++ AugmentingFunctionType.js
@@ -2,3 +2,7 @@ Function.prototype.method = function (name, func) {
 	this.prototype[name] = func;
 	return this;
 };
+Function.method("inherits", function (Parent) {
+	this.prototype = new Parent();
+	return this;
+});
